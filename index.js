e1fd4e1b0d74a6ac712f1a25dbeb2124b7585cef
salmamouri <mouriclub@gmail.com>
2022-10-25 18:55:57 +0600

    ctegories.json added

--- index.js
+++ index.js
@@ -2,8 +2,13 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 
+const categories = require("./data/categories.json");
+
 app.get("/", (req, res) => {
   res.send("Tech Learn API Running");
+});
+app.get("/categories", (req, res) => {
+  res.send(categories);
 });
 app.listen(port, () => {
   console.log("Tech Learn API is Running on port", port);
