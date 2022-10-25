d65a97b4d109105f0233c0ac496cbf5b05f19b30
salmamouri <mouriclub@gmail.com>
2022-10-25 18:38:30 +0600

    port 5000 created

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.send("Tech Learn API Running");
+});
+app.listen(port, () => {
+  console.log("Tech Learn API is Running on port", port);
+});
