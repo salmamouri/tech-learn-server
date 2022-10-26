3c387111f72bd7da514c6b14bad6f7290e03c092
salmamouri <mouriclub@gmail.com>
2022-10-26 18:07:21 +0600

    courses.json aded

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
+const cors = require("cors");
 
 const categories = require("./data/categories.json");
+const courses = require("./data/courses.json");
+
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Tech Learn API Running");
 });
 app.get("/categories", (req, res) => {
   res.send(categories);
+});
+app.get("/category/:id", (req, res) => {
+  const id = req.params.id;
+  const categoryCourse = courses.filter((c) => c.category_id === id);
+  res.send(categoryCourse);
+});
+app.get("/courses/:id", (req, res) => {
+  const id = req.params.id;
+  const selectCourse = courses.find((c) => c._id === id);
+  res.send(selectCourse);
 });
 app.listen(port, () => {
   console.log("Tech Learn API is Running on port", port);
